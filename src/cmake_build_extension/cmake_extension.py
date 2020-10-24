d31125bc225bb576b48190b895b66cd078790008
Diego Ferigo <diego.ferigo@iit.it>
2020-10-24 21:30:36 +0200

    Allow extending CMake prefix path

--- src/cmake_build_extension/cmake_extension.py
+++ src/cmake_build_extension/cmake_extension.py
@@ -1,3 +1,5 @@
+import os
+import importlib
 from typing import List
 from pathlib import Path
 from setuptools import Extension
@@ -14,6 +16,7 @@ class CMakeExtension(Extension):
         disable_editable: Skip this extension in editable mode.
         source_dir: The location of the main CMakeLists.txt.
         cmake_build_type: The default build type of the CMake project.
+        cmake_depends_on: List of dependency packages containing required CMake projects.
     """
 
     def __init__(self,
@@ -22,7 +25,8 @@ class CMakeExtension(Extension):
                  disable_editable: bool = False,
                  cmake_configure_options: List[str] = (),
                  source_dir: str = str(Path(".").absolute()),
-                 cmake_build_type: str = "Release"):
+                 cmake_build_type: str = "Release",
+                 cmake_depends_on: List[str] = ()):
 
         super().__init__(name=name, sources=[])
 
@@ -37,3 +41,27 @@ class CMakeExtension(Extension):
         self.disable_editable = disable_editable
         self.source_dir = str(Path(source_dir).absolute())
         self.cmake_configure_options = cmake_configure_options
+
+        for pkg in cmake_depends_on:
+
+            try:
+                importlib.import_module(pkg)
+            except ImportError:
+                raise ValueError(f"Failed to import '{pkg}'")
+
+            init = importlib.util.find_spec(pkg).origin
+            CMakeExtension.extend_cmake_prefix_path(path=str(Path(init).parent))
+
+    @staticmethod
+    def extend_cmake_prefix_path(path: str) -> None:
+
+        abs_path = Path(path).absolute()
+
+        if not abs_path.exists():
+            raise ValueError(f"Path {abs_path} does not exist")
+
+        if "CMAKE_PREFIX_PATH" in os.environ:
+            os.environ["CMAKE_PREFIX_PATH"] = \
+                f"{str(path)}:{os.environ['CMAKE_PREFIX_PATH']}"
+        else:
+            os.environ["CMAKE_PREFIX_PATH"] = str(path)
