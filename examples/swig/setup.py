7c5ebc070d384d1920f116916fa2ae68a74f460d
Diego Ferigo <diego.ferigo@iit.it>
2021-05-27 22:23:41 +0200

    Create a init file in the example to fix importing the C++ extension
    using Python >= 3.8 in Windows

--- examples/swig/setup.py
+++ examples/swig/setup.py
@@ -1,3 +1,4 @@
+import inspect
 import sys
 from pathlib import Path
 
@@ -7,11 +8,21 @@ from setuptools import setup
 with open(Path(__file__).parent.absolute() / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+init_py = inspect.cleandoc(
+    """
+    from cmake_build_extension import build_extension_env
+    
+    with build_extension_env():
+        from . import bindings
+    """
+)
+
 setup(
     ext_modules=[
         CMakeExtension(
             name="mymath",
             install_prefix="mymath",
+            write_top_level_init=init_py,
             source_dir=str(Path(__file__).parent.absolute()),
             cmake_configure_options=[
                 f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
